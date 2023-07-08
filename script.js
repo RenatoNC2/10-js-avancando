642c75d78e817ba5f99c193f7edec20e98d2e618
Renato Neres Costa <rnc5@aluno.ifnmg.edu.br>
2023-07-08 13:24:52 -0300

    ultimaalteração

--- script.js
+++ script.js
@@ -19,9 +19,40 @@ function inlista(n, l){
 function adicionar() {
     
     if (isNumero(num.value) && !inlista(num.value, valores)) {
-        window.alert('ok')
-
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicinoado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Insira o valor a lista/ valor invalido!')
     }
+    num.value = ''
+    num.focus()
+}
+function finalizar(){
+    if(valores.lengh == 0){
+        window.alert('adicione um valor')
+    }else{
+        let tot = valores.length
+        let maior =valores [0]
+        let menor = valores [0]
+        let media = 0
+        let soma = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if (valores[pos ]>maior)
+                maior = valores[pos]
+            if(valores[pos]<menor) 
+                menor = valores[pos] 
+        }   
+        media = soma / tot       
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} elementos.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${ maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p>A media dos valores é ${media}.</p>`
+    }
+    
 }
